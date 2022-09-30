d8a5add157dbce8a44b93f408c5e597bfb1b6a83
Ian <Naiguy1@users.noreply.github.com>
2022-09-30 09:33:54 -0600

    Add files via upload

---
+++ cursed.py
@@ -0,0 +1,23 @@
+def _4w0erh7f():
+    docstring = "The function takes no arguments, it writes your input to a .py file"
+    with open('b69f221c988f1d891fc98ff1ce012387.py', 'a') as ___:
+        _int = input(str("yes?:"))
+        if _int == 'yes':
+            _bool = []
+            while _int:
+                output = input('Input words:')
+                _bool.append(output)
+                ___.write(f'{_bool}')
+                comment = "Use ctrl+c to stop the program :)"
+        else: 
+            print('g'+
+            'o','com'+'mi'+
+            't', 'die')
+        comment = "Don't worry about the cleanliness of my code, I'm a beginner'"
+def k():
+    with open('b69f221c988f1d891fc98ff1ce012387.py', 'w') as __:
+        __.write(' ')
+        ky = "keep yourself"
+        comment = "This code will clear the file if you run it"
+_4w0erh7f()
+
